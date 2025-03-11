07113d950101bcbf59e1bf93993e69292e18fe88
Shavkat-F <fudjiyama@icloud.com>
2025-03-11 14:49:43 +0500

    Update practice6.sql

--- lesson-6/homework/practice6.sql
+++ lesson-6/homework/practice6.sql
@@ -2,8 +2,6 @@ select distinct col1, col2 from InputTbl
 except
 select distinct col1, col2 from InputTbl where col1>col2
 	
-select distinct col1, col2 from InputTbl where col1<col2
-	
 SELECT MIN(col1) AS col1, MAX(col2) AS col2
 FROM InputTbl
 GROUP BY 
@@ -13,6 +11,8 @@ GROUP BY
 select  Typ, count (*) as Counts from GroupbyMultipleColumns where Value1 = 'a' or Value2 = 'a' or Value3 = 'a'
 group by Typ
 
+select * from [dbo].[TestMultipleZero] where A!= '0' OR B!= '0' OR C!= '0' OR D!= '0'
+	
 select Name, 
 	count (case Fruit when 'Mango' then 1 else null end) as MangoCount,
 	count (case Fruit when 'Apple' then 1 else NULL end) as APPLECount,
