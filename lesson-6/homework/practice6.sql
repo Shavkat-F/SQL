b437adb65812244dfad7ce2c1245149e5918e80a
Shavkat-F <fudjiyama@icloud.com>
2025-03-11 14:40:57 +0500

    Update practice6.sql

--- lesson-6/homework/practice6.sql
+++ lesson-6/homework/practice6.sql
@@ -1,20 +1,17 @@
 select distinct col1, col2 from InputTbl
 except
 select distinct col1, col2 from InputTbl where col1>col2
-
+	
 select distinct col1, col2 from InputTbl where col1<col2
-
+	
 SELECT MIN(col1) AS col1, MAX(col2) AS col2
 FROM InputTbl
 GROUP BY 
     CASE WHEN col1 < col2 THEN col1 ELSE col2 END,
     CASE WHEN col1 < col2 THEN col2 ELSE col1 END;
 
-select * from GroupbyMultipleColumns
-
 select  Typ, count (*) as Counts from GroupbyMultipleColumns where Value1 = 'a' or Value2 = 'a' or Value3 = 'a'
 group by Typ
-
 
 select Name, 
 	count (case Fruit when 'Mango' then 1 else null end) as MangoCount,
